68ccd6568a7d69a9301716eb39423a1a6f535428
Rachel Misbin <rachelmisbin@pitt.edu>
2020-02-01 11:25:51 -0500

    added comments

--- software/include/Sensor.hpp
+++ software/include/Sensor.hpp
@@ -11,40 +11,48 @@ class Sensor
         Sensor() {}
         ~Sensor() {}
 
-        /* 
-         * Initialize the sensor so it can be used. Return true if the 
-         * sensor is initialized and ready to be enabled, disabled, read, or 
-         * polled, and false otherwise.
-         */
+        /****************************************************************************************** 
+         * Initialize the sensor so it can be used. Return true if the sensor is initialized and 
+         * ready to be enabled, disabled, read, or polled, and false otherwise.
+         * 
+         * @return true if successfully initialized, otherwise false
+         *****************************************************************************************/
         virtual bool init() = 0;
 
-        /*
-         * Read current sensor data and return it as vector of floats. 
-         * Accepts a data struct and populates the appropriate fields.
-         */
+        /******************************************************************************************
+         * Read current sensor data and returns it as Data struct. Accepts a data struct and 
+         * populates the appropriate fields.
+         * 
+         * @param data the struct to be populated
+         * @return Data populated from latest read
+         *****************************************************************************************/
         virtual Data read(Data data) = 0;
 
-        /*
-         * Accepts a data struct and populates the appropriate fields.
-         */
+        /******************************************************************************************
+         * Accepts a data struct and populates the appropriate fields from the latest call to 
+         * read().
+         * 
+         * @param data the struct to be populated
+         * @return Data populated from latest read
+         *****************************************************************************************/
         virtual Data poll(Data data) = 0;
 
-        /*
+        /******************************************************************************************
          * Enable the sensor to be read.
-         */
+         *****************************************************************************************/
         virtual void enable() = 0;
 
-        /*
+        /******************************************************************************************
          * Disable the sensor from being read.
-         */
+         *****************************************************************************************/
         virtual void disable() = 0;
     
     protected:
 
-        /*
+        /******************************************************************************************
          * Maintains the state of the sensor. It can only be enabled or disabled.
          * Only if the sensor is enabled ca the read() and poll() functions be called.
-         */
+         *****************************************************************************************/
         enum sensor_state { ENABLED, DISABLED };
         sensor_state state;
 };
